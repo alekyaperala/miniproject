f2e886a6db27c9835ba3455dc637dd22834235f8
alekyaperala <alekyaperala@gmail.com>
2024-02-13 05:43:56 +0000

    setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// App.js
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {indexPattern} from './Routes'
+import Header from './components/Header'
+import Home from './components/Home'
+
+const App = () => {
+  console.log('test')
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <Routes>
+          <Route path={indexPattern} element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
